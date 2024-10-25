58f00d3b9bb02f2f4351962a5348d8817c9f6ba9
Thabo Tshabalala <104504536+47thabo@users.noreply.github.com>
2024-10-25 20:18:54 +0200

    FEAT: add page cards to the dashboard

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Area_Chart } from '@/components/charts/Area_Chart';
 import { Bar_Chart_Horizontal } from '@/components/charts/Bar_Chart_Horizontal';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Package, Grid, ShoppingCart, Users } from "lucide-react"
+
 const columns = [
   {
     accessorKey: "status",
@@ -21,11 +24,16 @@ const AdminDashboard = () => {
     // setData(fetchedData)
   }, []);
 
+  const overviewCards = [
+    { title: "Active Users", value: "10,543", icon: Users },
+    { title: "Total Products", value: "1,234", icon: Package },
+    { title: "Categories", value: "25", icon: Grid },
+    { title: "Total Orders", value: "3,456", icon: ShoppingCart },
+  ];
+
   return (
     <div className="p-4">
-      <h2 className="text-2xl mb-4">Admin Dashboard</h2>
-      <p className="mb-4">Welcome to the admin dashboard! Only accessible by admin users.</p>
-
+      <h2 className="text-2xl mb-4">Dashboard</h2>
       <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         <div className="mb-8 w-full">
           <Area_Chart />
@@ -34,7 +42,21 @@ const AdminDashboard = () => {
           <Bar_Chart_Horizontal />
         </div>
       </div>
-
+      <section className="mb-6 space-y-6">
+        <div className="grid gap-2 sm:grid-cols-2 lg:grid-cols-4">
+          {overviewCards.map((card, index) => (
+            <Card className="py-4 transition-all duration-300 ease-in-out hover:shadow-lg hover:-translate-y-1 cursor-pointer" key={index}>
+              <div className="flex flex-row items-center justify-between space-y-0 px-6 pb-2">
+                <CardTitle className="text-sm font-medium">{card.title}</CardTitle>
+                <card.icon className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div className='px-6'>
+                <div className="text-2xl font-bold">{card.value}</div>
+              </div>
+            </Card>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
