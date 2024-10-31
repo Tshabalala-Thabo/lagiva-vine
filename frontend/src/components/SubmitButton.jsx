5c0318ce0d02656f2dd926e0b88935a58ed2090b
Thabo Tshabalala <47thabo@gmail.com>
2024-10-31 16:37:40 +0200

    FIX: submit button not working on forms

--- frontend/src/components/SubmitButton.jsx
+++ frontend/src/components/SubmitButton.jsx
@@ -3,7 +3,7 @@ import { PulseLoader } from 'react-spinners' // Updated to import PulseLoader
 
 const SubmitButton = ({ loading, text = 'Submit', width = 'w-24', onClick }) => ( // Added width prop with default value and onClick prop
     <button
-        type="button" // Ensure this is set to "button" to prevent form submission
+        type="submit" // Ensure this is set to "button" to prevent form submission
         className={`bg-blue-primary text-white px-4 py-2 rounded ${loading ? 'opacity-50 cursor-not-allowed' : ''} ${width}`} // Use the width prop
         disabled={loading} // Disable button while loading
         onClick={onClick} // Ensure onClick is passed to the button
