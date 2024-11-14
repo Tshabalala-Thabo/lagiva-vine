06d4c37e40d93d1e85def9f5eb89641a6f97274b
Thabo Tshabalala <47thabo@gmail.com>
2024-11-14 18:17:03 +0200

    FIX: api import

--- frontend/src/hooks/usePublishedProducts.js
+++ frontend/src/hooks/usePublishedProducts.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from '../config/axiosConfig'; // Import the configured Axios instance
+import api from '../config/axiosConfig'; // Import the configured Axios instance
 
 const usePublishedProducts = () => {
   const [publishedProducts, setPublishedProducts] = useState([]);
