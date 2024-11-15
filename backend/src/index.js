11bfe57017d00029688b948aa8d109336f1ee34c
Thabo Tshabalala <104504536+47thabo@users.noreply.github.com>
2024-11-15 21:47:26 +0200

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import productRoutes from '../src/routes/productRoutes.js'
 import authRoutes from '../src/routes/authRoutes.js' // Import the Auth routes
 import categoryRoutes from '../src/routes/categoryRoutes.js' // Import the Category routes
 import userRoutes from './routes/userRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 
 dotenv.config()
 
@@ -20,7 +21,7 @@ connectDB()
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello from the backend!')
+  res.send('Hello from the backend!!!')
 })
 
 // Use product routes
@@ -34,6 +35,9 @@ app.use('/api/categories', categoryRoutes) // Added category routes
 
 // Use user routes
 app.use('/api/users', userRoutes);
+
+// Use cart routes
+app.use('/api/cart', cartRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
